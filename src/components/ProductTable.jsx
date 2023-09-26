5beaf0c84c363bded78e8a20df3b0eaff03f281b
tk <trishank01@gmail.com>
2023-09-26 21:31:50 +0530

    UI done

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,10 +1,11 @@
 import Search from "antd/es/input/Search";
 import { AiOutlinePrinter } from "react-icons/ai";
+import TableBox from "./Table";
 
 const ProductTable = () => {
   return (
     <>
-      <div className="flex bg-white border-[1px] border-solid rounded-lg mt-12 p-[30px] border-[#b8b5b5] shadow-sm ">
+      <div className="bg-white border-[1px] border-solid rounded-lg mt-12 p-[30px] border-[#b8b5b5] shadow-sm ">
         <div className="flex justify-between">
           <div>
             <Search
@@ -13,12 +14,15 @@ const ProductTable = () => {
               className=""
             />
           </div>
-          <div>
-            <button>Add item</button>
+          <div className="flex items-center gap-5">
+          <button className="border-2 border-solid border-green-700 py-2 px-3 bg-white rounded-2xl text-green-600 font-bold cursor-pointer">Add item</button>
             <div>
-              <AiOutlinePrinter />
+              <AiOutlinePrinter size={30} color="green"/>
             </div>
           </div>
+        </div>
+        <div>
+            <TableBox/>
         </div>
       </div>
     </>
